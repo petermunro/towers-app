1f6f0591b1e8e900db44f03870be4ac73409c72b
Peter Munro <peter@petermunro.org>
2021-07-20 00:56:27 +0100

    Added devtools

--- src/store.js
+++ src/store.js
@@ -1,4 +1,7 @@
 import { createStore } from "redux";
 import { towersReducer } from "./towersReducer";
 
-export const store = createStore(towersReducer);
+export const store = createStore(
+  towersReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
